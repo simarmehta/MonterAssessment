2cc8098e4dd5adab5886a4f65cd74c2a88dc7ee1
simar <mehtasimar2015@gmail.com>
2024-04-20 16:10:04 -0400

    server commit

---
+++ server.js
@@ -0,0 +1,65 @@
+// server.js
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('./models/User');
+const transporter = require('./config/emailConfig');
+
+const app = express();
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/myapp')
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log('Failed to connect to MongoDB', err));
+
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the Authentication API!');
+});
+
+app.post('/register', async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).send('Email already registered');
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const otp = Math.floor(100000 + Math.random() * 900000); // Generate OTP
+    const user = new User({ email, password: hashedPassword, isVerified: false, otp: otp });
+    await user.save();
+    await transporter.sendMail({
+      from: process.env.EMAIL_USERNAME,
+      to: email,
+      subject: 'Verify Your Email',
+      text: `Your OTP for email verification is: ${otp}`
+    });
+    res.status(201).send('User registered. Please verify your email.');
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+app.post('/login', async (req, res) => {
+  try {
+    console.log('Request Headers:', req.headers);
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user || !await bcrypt.compare(password, user.password)) {
+      return res.status(401).send('Invalid credentials');
+    }
+    if (!user.isVerified) {
+      return res.status(403).send('Please verify your email first');
+    }
+    const JWT_SECRET = '383290605e178261b3c96a6209859742f6d2a78c79617b3ed7bccabecedd72b0dd6c81b69c49315f1dca94777f650a68836ade42fa955fe55a231f00f07bfeae';
+    console.log('JWT Secret Key:', JWT_SECRET);
+    const token = jwt.sign({ userId: user._id },JWT_SECRET, { expiresIn: '1h' });
+    res.status(200).send({ token });
+    
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
